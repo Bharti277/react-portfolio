ee7acd87664f8b921c97041c5e7f70cfa74fa85d
Bharti277 <bhartideependra277@gmail.com>
2022-12-23 11:46:41 +0530

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,4 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 
@@ -13,74 +8,19 @@ function ResponsiveAppBar() {
  
 
   return (
-    <AppBar className='navbar_container'>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-         <Box>
-         <Typography
-          >
-            deependra
-          </Typography>
-         </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Home
-          </Typography>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/about"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            About
-          </Typography>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/projects"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Projects
-          </Typography>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-          <NavLink to='#' className="">
-            <button className=''>Contact me</button>
-           </NavLink>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <div className='navbar_container'>
+     <div className="logo">
+            <NavLink to='/'><button className='btn'>My Work</button></NavLink>
+     </div>
+     <div className="navlinks">
+     <NavLink to='/'><button className='btn'>My Work</button></NavLink>
+     <NavLink to='/About'><button className='btn'>My Work</button></NavLink>
+     <NavLink to='/Projects'><button className='btn'>My Work</button></NavLink>
+     </div>
+     <div className="contact">
+     <NavLink to='#'><button className='btn'>My Work</button></NavLink>
+     </div>
+    </div>
   );
 }
 export default ResponsiveAppBar;
