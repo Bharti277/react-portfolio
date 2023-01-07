f0ff6eaf94172c19e0509db75c4d7f6470e448b9
Bharti277 <bhartideependra277@gmail.com>
2023-01-07 19:52:46 +0530

    wip

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@
     live: "https://bharti277.github.io/order-catalogue/",
     source: "https://github.com/Bharti277/order-catalogue",
     description:  
-        "Order catalgue is a e-commerce cart website in which user can add items to cart.",
+        "Portfolio is a e-commerce cart website in which user can add items to cart.",
   },
   {
     id: "1",
