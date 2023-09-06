c86ec2e3413c4e75ceff400867b38723d8e313bf
Bharti277 <bhartideependra277@gmail.com>
2023-09-06 11:07:19 +0530

    contact style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,15 +29,25 @@ const Contact = () => {
 
   return (
     <div className="contact_container">
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+      <div className="contact_form">
+        <form ref={form} onSubmit={sendEmail} className="form">
+          <p>Leave your message here!</p>
+
+          <div>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" name="user_email" />
+          </div>
+          <div>
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
