9c26bb37b409ee02df224a977b9b09cea6d31038
Bharti277 <bhartideependra277@gmail.com>
2023-09-07 14:12:06 +0530

    wip

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,36 +1,49 @@
-import React from 'react'
-import "./Project.css"
- 
-function Project({id, image, name, stack, live, source, desc}) {
+import React from "react";
+import "./Project.css";
+
+function Project({ id, image, name, stack, live, source, desc }) {
   return (
     <div className="project_container">
-        <div className="card mb-5" style={{maxWidth:'900px'}}>
+      <div className="card mb-5" style={{ maxWidth: "450px" }}>
         <div className="g-0 d-flex flex-column flex-lg-row align-items-center ">
-            <div className="">
-                <img src={image} className="img-fluid rounded-start project-img" alt={name} />
+          <div className="">
+            <img
+              src={image}
+              className="img-fluid rounded-start project-img"
+              alt={name}
+            />
+          </div>
+          <div className="">
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+              <p className="card-text">{desc}</p>
+              <div className="mt-5 align-text-bottom">
+                <p className="card-text">{stack}</p>
+                <p className="card-text d-flex gap-4">
+                  <a
+                    href={live}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="link"
+                  >
+                    Live Site
+                  </a>
+                  <a
+                    href={source}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="link"
+                  >
+                    Source Code
+                  </a>
+                </p>
+              </div>
             </div>
-            <div className="">
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{desc}</p>
-                    <div className="mt-5 align-text-bottom">
-                        <p className="card-text">{stack}</p>
-                            <p className="card-text d-flex gap-4">
-                                <a href={live} target="_blank" rel="noreferrer" className='link'>
-                                    Live Site 
-                                </a>
-                                <a href={source} target="_blank" rel="noreferrer" className='link'>
-                                    Source Code 
-                                </a>    
-                            </p>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Project
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,35 @@
-import React from 'react'
-import projectsData from '../data.js'
-import Project from './Project.js'
+import React from "react";
+import projectsData from "../data.js";
+import Project from "./Project.js";
 
 const Projects = () => {
   return (
-    <div className="container-fluid bg-light p-5 min-vh-100">
+    <div
+      className="container-fluid bg-light p-5 min-vh-100"
+      style={{ paddingTop: "8rem" }}
+    >
       <div className="row">
         {projectsData.map((project) => {
-          return(
-            <div className="col-12 d-flex justify-content-center" key={project.id}>
-                <Project id={project.id}
-                  image={project.img}
-                  name={project.name}
-                  stack={project.stack}
-                  live={project.live}
-                  source={project.source}
-                  desc={project.description}
-                />
+          return (
+            <div
+              className="col-12 d-flex justify-content-center"
+              key={project.id}
+            >
+              <Project
+                id={project.id}
+                image={project.img}
+                name={project.name}
+                stack={project.stack}
+                live={project.live}
+                source={project.source}
+                desc={project.description}
+              />
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
