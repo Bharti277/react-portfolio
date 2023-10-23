eecb91b0c0297f927471608c0033256a4a72d1d0
Bharti277 <bhartideependra277@gmail.com>
2023-10-23 13:12:31 +0530

    wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 function ResponsiveAppBar() {
   const [showMediaIcons, setShowIcons] = useState(false);
+  console.log("show");
   const showMediaIconsHandler = () => {
     setShowIcons(!showMediaIcons);
   };
