109c97dadbd1cccbc3093f7a7736ae812dcf454a
Bharti277 <bhartideependra277@gmail.com>
2025-05-09 17:45:48 +0530

    new design wip

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { ArrowUp } from "lucide-react";
+
+export const Footer = () => {
+  return (
+    <footer className="relative flex flex-wrap items-center justify-between px-4 py-12 pt-8 mt-12 border-t bg-card border-border">
+      {" "}
+      <p className="text-sm text-center text-muted-foreground">
+        {" "}
+        &copy; {new Date().getFullYear()} All rights reserved.
+      </p>
+      <a
+        href="#hero"
+        className="p-2 transition-colors rounded-full bg-primary/10 hover:bg-primary/20 text-primary"
+      >
+        <ArrowUp size={20} />
+      </a>
+    </footer>
+  );
+};
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,49 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative flex flex-col items-center justify-center min-h-screen px-4"
+    >
+      <div className="container z-10 max-w-4xl mx-auto text-center">
+        <div className="space-y-6">
+          <h1 className="text-4xl font-bold tracking-tight md:text-6xl">
+            <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+            <span className="opacity-0 text-primary animate-fade-in-delay-1">
+              {" "}
+              Deependra
+            </span>
+            <span className="ml-2 opacity-0 text-gradient animate-fade-in-delay-2">
+              {" "}
+              Bharti
+            </span>
+          </h1>
+
+          <p className="mx-auto text-lg opacity-0 md:text-xl text-muted-foreground max-2-2xl animate-fade-in-delay-3">
+            I have 4+ years of hands-on experience in web development and My
+            motivation to learn different technologies and algorithms drives me.
+            I love the challenges of a steep learning curve, which makes me a
+            better learner. I strongly desire to learn different technologies,
+            which would enable me to become a better developer. I believe in the
+            fast execution culture while building things from scratch. I believe
+            in startups where the learning process is very high and focuses on
+            improving new ideas. I build interfaces that are both beautiful and
+            functional.
+          </p>
+
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute flex flex-col items-center transform -translate-x-1/2 bottom-8 left-1/2 animate-bounce">
+        <span className="mb-2 text-sm text-muted-foreground"> Scroll </span>
+        <ArrowDown className="w-5 h-5 text-primary" />
+      </div>
+    </section>
+  );
+};
